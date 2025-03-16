48c1ef2ae20e2c30208becd9d5b994568f2b7542
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2025-03-16 18:59:19 +0500

    Debugged the warning message for mongoose setup

--- microservices/iam/app.js
+++ microservices/iam/app.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
